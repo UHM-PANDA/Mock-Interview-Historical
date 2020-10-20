800623630a5985924f3998a4f0ca91606fd88d65
kanaigooding <65993942+kanaigooding@users.noreply.github.com>
2020-10-19 22:19:22 -1000

    Week6 Problem 1: Add

--- Fall 2020/Week_06/02_RPN/Cpp/main.cpp
+++ Fall 2020/Week_06/02_RPN/Cpp/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
-#include <string>
-
-#include "rpn.hpp"
+#include <vector>
 
 int main(int argc, char** argv){
-
-    std::string temp = "53-4+5*";
-
-    std::cout << rpn(temp) << std::endl;
 
     return 0;
 }
--- Fall 2020/Week_06/02_RPN/Java/main.java
+++ Fall 2020/Week_06/02_RPN/Java/main.java
@@ -1,3 +1,37 @@
-public class main {
-     
+public class Main {
+
+     public int reversePolishNotation(Char[] arr) {
+
+          int returnValue = 0;
+          String operators = "+-*/";
+
+          Stack<Char> stack new Stack<Char>();
+
+          for (char t: arr) {
+               if (!operators.contains(t)) {
+                    stack.push(t);
+               } else {
+                    int a = Integer.valueOf(stack.pop());
+                    int b = Integer.valueOf(stack.pop());
+                    switch(t) {
+                    case "+":
+                         stack.push(String.valueOf(b+a));
+                         break;
+                    case "-":
+                         stack.push(String.valueOf(b-a));
+                         break;
+                    case "*":
+                         stack.push(String.valueOf(b * a));
+                         break;
+                    case "/":
+                         stack.push(String.valueOf(b/a));
+                         break;
+                    }
+               }
+          }
+          returnValue = Integer.valueOf(stack.pop());
+
+          return returnValue;
+     }
+
 }
